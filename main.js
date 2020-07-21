1d43ca2777db6c7e30944d7479eb051e4bcb4afd
Damion Wiltshire <drw.5452@gmail.com>
2020-07-21 12:55:02 -0700

    Show blank overlay when question cell is clicked

--- main.js
+++ main.js
@@ -1,3 +1,23 @@
+/* Confirms this file is connected to index.html */
 const jsGreeting = document.createElement('p');
 jsGreeting.innerText = "main.js is connected";
 document.body.prepend(jsGreeting);
+
+/* Adds click events to each cell */
+function addClickEvents() {
+    const questionCells = document.querySelectorAll('.ques');
+    questionCells.forEach((el) => {
+        el.addEventListener('click', showQuestion);
+    });
+}
+
+function showQuestion() {
+    alert(`I'm supposed to show a question`);
+    const overlay = document.querySelector('#overlay');
+    overlay.style.display = 'block';
+    overlay.addEventListener('click', (e)=> {
+        overlay.style.display = 'none';
+    });
+}
+
+addClickEvents();
